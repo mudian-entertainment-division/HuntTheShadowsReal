1505cfefb090cf9c058bdc294cd6d2fb03467ef4
JamesNemeth <53249899+JamesNemeth@users.noreply.github.com>
2019-09-04 14:16:41 +1000

    push 2
    few changes

---
+++ Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHandler : MonoBehaviour
+{
+    [Header("Value Variables")]
+
+    public float curHealth;
+    public float curMana, maxHealth, maxMana;
+    [Header("Value Variables")]
+
+    public Slider healthBar;
+    public Slider manaBar;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (healthBar.value != Mathf.Clamp01(curHealth / maxHealth))
+        {
+            curHealth = Mathf.Clamp(curHealth, 0, maxHealth);
+            healthBar.value = Mathf.Clamp01(curHealth / maxHealth);
+        }
+        if (manaBar.value != Mathf.Clamp01(curMana / maxMana))
+        {
+            curMana = Mathf.Clamp(curMana, 0, maxHealth);
+            manaBar.value = Mathf.Clamp01(curMana / maxMana);
+        }
+    }
+}
