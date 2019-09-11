23d4a82f4fb24d9c8a7f5177d8367d2f65c04da7
JamesNemeth <53249899+JamesNemeth@users.noreply.github.com>
2019-09-11 13:31:22 +1000

    adding in the UI

---
+++ Assets/Scripts/RadialHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialHealth : MonoBehaviour
+{
+    public Image radialHealthIcon;
+    public float curHealth, maxHealth;
+   
+    void HealthChange()
+    {
+        float amount = Mathf.Clamp01(curHealth / maxHealth);
+        radialHealthIcon.fillAmount = amount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthChange();
+    }
+}
