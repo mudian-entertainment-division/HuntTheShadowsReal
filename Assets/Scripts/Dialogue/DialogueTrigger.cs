f16472e3f6f3c481bfdd8c3a8c29a01dd81592cb
Jedde1 <54158726+Jedde1@users.noreply.github.com>
2019-09-26 09:44:08 +1000

    ugf

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -8,6 +8,6 @@ public class DialogueTrigger : MonoBehaviour
 
     public void TriggerDialogue()
     {
-        FindObjectOfType<DialogueManager>().StartDialogue(Dialogue);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 }
---
+++ Assets/Scripts/OnTriggerEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class OnTriggerEvent : MonoBehaviour
+{
+    public UnityEvent onEnter;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger");
+
+        //Triggers events for unlocking the door
+        if (other.tag == "Player")
+        {
+            onEnter.Invoke();
+            Debug.Log("Next Level");
+        }
+    }
+}
